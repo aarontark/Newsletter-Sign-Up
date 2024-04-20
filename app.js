48743576d845921117adc6366b0256cd44ed416c
aarontark <tarkington.aaron@gmail.com>
2024-04-20 02:05:58 -0500

    added event listeners for buttons

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const confirmationCard = document.querySelector('.confirmation-card');
 
 emailInput.addEventListener('invalid', () => {
     errorMsg.style.opacity = '1';
+
     if (emailInput.classList.contains('input-box')) {
         emailInput.classList.remove('input-box');
         emailInput.classList.add('input-box-error');
@@ -21,6 +22,16 @@ emailForm.addEventListener('submit', () => {
     event.preventDefault();
     registerCard.style.display = 'none';
     confirmationCard.style.display = 'flex';
+    emailHighlight.innerHTML = emailInput.value;
+})
+
+dismissBtn.addEventListener('click', () => {
+    if (window.innerWidth >= 1120) {
+        registerCard.style.display = 'grid';
+    } else {
+        registerCard.style.display = 'block';
+    }
+    confirmationCard.style.display = 'none';
 })
 
 
